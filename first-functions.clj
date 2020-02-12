6317c7da71f7a9a0e5c1d087dd08128897df0c7e
Paulo <paulonunes@natura.net>
2020-02-11 23:41:36 -0300

    local variables and using conditional

--- first-functions.clj
+++ first-functions.clj
@@ -6,8 +6,13 @@
 (show-message)
 
 (defn apply-discount 
-    "Apply a discount according to the amount and percentage."
-    [percentage gross-amount]
-    (* gross-amount percentage))
+    "Apply a discount according to the amount and percentage, if gross amount is bigger than 100"
+    [gross-amount]
+
+    (if (> gross-amount 100)
+        (let [percentage 0.10]
+            (println "Apllying discount of " percentage)
+            (* gross-amount percentage))
+        gross-amount))
 
 (apply-discount 100)
